1c3ed7eecdfd55bed528649985f0c20d93c063e1
saikishan <saikishan2008@gmail.com>
2018-03-27 21:08:44 +0530

    i have resolved the issues ithe login endpoint and registration and made few few required changes in the db schema

--- schema.sql
+++ schema.sql
@@ -1,111 +1,78 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
---
--- Host: localhost    Database: pratikriya
--- ------------------------------------------------------
--- Server version	5.7.21-0ubuntu0.16.04.1
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
---
--- Table structure for table `organizations`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema pratikriya
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `organizations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `organizations` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `country` varchar(100) NOT NULL,
-  `zip` varchar(10) NOT NULL,
-  `adminid` int(11) DEFAULT NULL,
+-- -----------------------------------------------------
+-- Schema pratikriya
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pratikriya` DEFAULT CHARACTER SET utf8 ;
+USE `pratikriya` ;
+
+-- -----------------------------------------------------
+-- Table `pratikriya`.`roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pratikriya`.`roles` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(30) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `fk_admin` (`adminid`),
-  CONSTRAINT `organizations_ibfk_1` FOREIGN KEY (`adminid`) REFERENCES `users` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  INDEX `roleindex` (`ID` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1;
 
---
--- Dumping data for table `organizations`
---
 
-LOCK TABLES `organizations` WRITE;
-/*!40000 ALTER TABLE `organizations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `organizations` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roles` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(30) DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `pratikriya`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pratikriya`.`users` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `phone` VARCHAR(20) NOT NULL,
+  `roleid` INT(11) NULL DEFAULT NULL,
+  `designation` VARCHAR(255) NULL DEFAULT NULL,
+  `password` VARCHAR(200) NOT NULL,
+  `Oid` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `roleindex` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  INDEX `fk_role` (`roleid` ASC),
+  CONSTRAINT `users_ibfk_1`
+    FOREIGN KEY (`roleid`)
+    REFERENCES `pratikriya`.`roles` (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 21
+DEFAULT CHARACTER SET = latin1;
 
---
--- Dumping data for table `roles`
---
 
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'Domain Admin'),(2,'Form Creator'),(3,'Application User');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `roleid` int(11) DEFAULT NULL,
-  `designation` varchar(255) DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `pratikriya`.`organizations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pratikriya`.`organizations` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `country` VARCHAR(100) NOT NULL,
+  `zip` VARCHAR(10) NOT NULL,
+  `adminid` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `fk_role` (`roleid`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`roleid`) REFERENCES `roles` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  INDEX `fk_admin` (`adminid` ASC),
+  CONSTRAINT `organizations_ibfk_1`
+    FOREIGN KEY (`adminid`)
+    REFERENCES `pratikriya`.`users` (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = latin1;
 
---
--- Dumping data for table `users`
---
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-03-27  1:38:36
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -2,163 +2,189 @@ var _ = require('lodash');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var Q = require('q');
-const mysqlcon = require("../config/mysqlcon")
+var config = require('../../config.json')
+const mysqlcon = require("../config/mysqlconn")
 
 var service = {};
 
 service.authenticate = authenticate;
+//service.getAll = getAll;
 service.getById = getById;
 service.create = create;
 service.update = update;
+service.delete = _delete;
 
 module.exports = service;
 
 function authenticate(email, password) {
-    var deferred = Q.defer();
-    mysqlcon.query('select ID, email, password, name from users where email = ?', [email], function (err, rows) {
-        if (err) deferred.reject(err.name + ': ' + err.message)
+  var deferred = Q.defer();
+  mysqlcon.query('select ID, email, password, name from users where email = ?', [email], function (err, rows) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
 
-        if (rows.length && bcrypt.compareSync(password, rows[1].password)) {
-            deferred.resolve({
-                _id: rows[1].ID,
-                email: email,
-                name: rows[1].name,
-                token: jwt.sign({ sub: rows[1].ID }, config.secret)
-            });
-        } else {
-            // user not found
-            deferred.resolve();
-        }
-    })
+    if (rows.length && bcrypt.compareSync(password, rows[0].password)) {
+      deferred.resolve({
+        _id: rows[0].ID,
+        email: email,
+        name: rows[0].name,
+        token: jwt.sign({ sub: rows[0].ID }, config.secret)
+      });
+    } else {
+      // user not found
+      deferred.resolve();
+    }
+  })
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function getById(_id) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    mysqlcon.query('select * from users where ID = ?', [_id], function (err, rows) {
-        if (err) deferred.reject(err.name + ': ' + err.message)
+  mysqlcon.query('select * from users where ID = ?', [_id], function (err, rows) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
 
-        if (rows.length) {
-            // return user (without hashed password)
-            deferred.resolve(_.omit(rows[1], 'password'));
-        } else {
-            // user not found
-            deferred.resolve();
-        }
-    })
+    if (rows.length) {
+      // return user (without hashed password)
+      deferred.resolve(_.omit(rows[0], 'password'));
+    } else {
+      // user not found
+      deferred.resolve();
+    }
+  })
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function create(userParam) {
-    var deferred = Q.defer();
-
-    // validation
+  var deferred = Q.defer();
+  // validation
+  bcrypt.hash(userParam.password, 10, function (err, hashpassword){
     mysqlcon.query('select ID from users where email = ?', [userParam.email], function (err, rows) {
-        if (err) deferred.reject(err.name + ': ' + err.message)
+      if (err) deferred.reject(err.name + ': ' + err.message)
+      if (rows.length) {
+        // return user (without hashed password)
+        deferred.reject('Username "' + userParam.email + '" is already taken');
+      } else {
+        // user not found
+        if (userParam.role == 1) { //validate organization
+          mysqlcon.query('select ID from organizations where name = ?', [userParam.organization.name], function (err, rows) {
+            if (err) deferred.reject(err.name + ': ' + err.message)
 
-        if (rows.length) {
-            // return user (without hashed password)
-            deferred.reject('Username "' + userParam.email + '" is already taken');
-        } else {
-            // user not found
-            if (userParam.role == 1) { //validate organization
-                mysqlcon.query('select ID from organizations where name = ?', [userParam.organization.name], function (err, rows) {
-                    if (err) deferred.reject(err.name + ': ' + err.message)
-
-                    if (rows.length) {
-                        deferred.reject('Organization "' + userParam.organization.name + '" is already registered under different admin');
-                    } else {
-                        // organization not found
-                        createAdminUser();
-                    }
-                })
+            if (rows.length) {
+              deferred.reject('Organization "' + userParam.organization.name + '" is already registered under different admin');
             } else {
-                // user not found
-                createUser();
+              // organization not found
+              createAdminUser(hashpassword);
             }
+          })
+        } else {
+          // user not found
+          createUser(hashpassword);
         }
+      }
+    })
+  })
+
+  function createAdminUser(password) {
+
+    if (userParam.role == 1) {
+      //insert the user
+      mysqlcon.query('insert into users (name,email,phone,roleid,designation,password) values(?,?,?,?,?,?)',[userParam.name,userParam.email,userParam.phone,userParam.role,userParam.designation,password],function(err,rows){
+        if(err){
+          deferred.reject(err.name)
+        }
+        else{
+          mysqlcon.query('select ID from users where email = ?', [userParam.email],function (err,rows) {
+            var adminid=rows[0].ID
+            mysqlcon.query('insert into organizations (name,address,city,country,zip,adminid) values(?,?,?,?,?,?)',[userParam.organization.name,userParam.organization.address,userParam.organization.city,userParam.organization.country,userParam.organization.zip,adminid],function(err,rows){
+              if(err) {
+                deferred.reject("failed in the creation of the orgazination")
+                mysqlcon.query('delete from users where ID=?',[rows[0].ID],function(err,result){})
+              }
+              else{
+
+                mysqlcon.query('select ID from organizations where adminid = ?',[adminid],function (err,rows) {
+                  if(err) deferred.reject("error in update of rows");
+                  else{
+                    mysqlcon.query('update users set Oid=? where ID= ?',[rows[0].ID,adminid],function (err,result) {
+                      if(err) {
+                        deferred.reject("error in updating the admin Oid")
+                      }
+                      else{
+                        deferred.resolve()
+                      }
+                    })
+                  }
+                })
+              }
+            })
+          })
+        }
+      })
+      // insert organization first
+      /*mysqlcon.query('insert into organizations (...) values (...)', [...], function (err, rows) {
+          //handle error
+          // if success
+          //do another query to insert user (or vice versa depending upon the foreign key constraint)
+          // add hashed password to user object
+          user.password = bcrypt.hashSync(userParam.password, 10);
+      })*/
+    }
+  }
+  function createUser(password) {
+    mysqlcon.query('insert into users (name,email,phone,roleid,designation,password,Oid) values(?,?,?,?,?,?,?)',[userParam.name,userParam.email,userParam.phone,userParam.role,userParam.designation,password,userParam.Oid],function(err,rows) {
+      if(err) deferred.reject("error in the creation if the user")
+      deferred.resolve()
     })
 
-
-
-    function createAdminUser() {
-
-        if (userParam.role == 1) {
-            // insert organization first
-            mysqlcon.query('insert into organizations (...) values (...)', [...], function (err, rows) {
-                //handle error
-                // if success
-                //do another query to insert user (or vice versa depending upon the foreign key constraint)
-                // add hashed password to user object
-                user.password = bcrypt.hashSync(userParam.password, 10);
-            })
-        }
-    }
-
-    function createUser() {
-
-        if (userParam.role == 1) {
-            // insert organization first
-            mysqlcon.query('insert into organizations (...) values (...)', [...], function (err, rows) {
-                //handle error
-                // if success
-                //do another query to insert user (or vice versa depending upon the foreign key constraint)
-                // add hashed password to user object
-                user.password = bcrypt.hashSync(userParam.password, 10);
-            })
-        }
-    }
-
-    return deferred.promise;
+  }
+  return deferred.promise
 }
 
 function update(_id, userParam) {
 
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    mysqlcon.query('select * from users where ID = ?', [_id], function (err, rows) {
-        if (err) deferred.reject(err.name + ': ' + err.message)
+  mysqlcon.query('select * from users where ID = ?', [_id], function (err, rows) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
 
-        if (rows.length) {
-            // return user (without hashed password)
-            updateUser();
-        } else {
-            // user not found
-            deferred.resolve();
-        }
-    })
-
-
-    function updateUser() {
-        // fields to update
-        // run update query
+    if (rows.length) {
+      // return user (without hashed password)
+      updateUser();
+    } else {
+      // user not found
+      deferred.resolve();
     }
+  })
 
-    return deferred.promise;
+
+  function updateUser() {
+    // fields to update
+    // run update query
+  }
+
+  return deferred.promise;
 }
 
 function _delete(_id) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    User.findById(_id, function (err, user) {
-        if (err) deferred.reject(err.name + ': ' + err.message);
+  User.findById(_id, function (err, user) {
+    if (err) deferred.reject(err.name + ': ' + err.message);
 
-        deleteUser(user);
-    });
+    deleteUser(user);
+  });
 
-    function deleteUser(user) {
-        // fields to update
-        var u = new User(user);
+  function deleteUser(user) {
+    // fields to update
+    var u = new User(user);
 
-        u.remove((err) => {
-            if (err) deferred.reject(err.name + ': ' + err.message);
+    u.remove(function(err){
+      if (err) deferred.reject(err.name + ': ' + err.message);
 
-            deferred.resolve();
-        });
-    }
+    deferred.resolve();
+  });
+  }
 
-    return deferred.promise;
+  return deferred.promise;
 }
