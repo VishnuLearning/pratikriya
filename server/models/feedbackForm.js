c585ad98db79772d6589497ec0fe3f35ce050c7d
VinayV9 <vinayveeren@gmail.com>
2018-03-29 15:18:05 +0530

    defined schema for form :)

--- server/models/feedbackForm.js
+++ server/models/feedbackForm.js
@@ -2,6 +2,27 @@ const mongoose = require("../config/mongodb");
 
 const feedbackForm = {
     //define schema
+    name : String,
+    createdBy: String,
+    sections: [
+        {
+            name: String,
+            questions: [
+                {
+                    text:String,
+                    name:String,
+                    type:String,
+                    attempted:Boolean,
+                    choices: [
+                        {
+                            text: String,
+                            score: Number
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 };
 
 module.exports = mongoose.model("FeedbackForm", feedbackForm);
