4cd108f9480b2ff121bce38afdcb79f79144c2a7
Abhinav Dayal <abhinav.dayal@vishnu.edu.in>
2018-03-30 20:34:38 -0700

    Added scaffolding for workflow management

---
+++ mainclient/app/components/workflow/managelaunch/managelaunch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managelaunch',
+  templateUrl: './managelaunch.component.html',
+  styleUrls: ['./managelaunch.component.scss']
+})
+export class ManagelaunchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
